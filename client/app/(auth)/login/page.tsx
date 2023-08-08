2ac1db5b476cd6d98c71007fbf66843e9d9a388c
Ayush Goyal <ayush_g@ar.iitr.ac.in>
2023-08-08 18:16:16 +0530

    starting to work on authenticaiton backend

--- client/app/(auth)/login/page.tsx
+++ client/app/(auth)/login/page.tsx
@@ -1,114 +1,106 @@
 "use client"
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { isDirty, isValid, z } from "zod";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Image from 'next/image';
+import { Separator } from "@/components/ui/separator";
+import { DevTool } from "@hookform/devtools";
 
-import Link from "next/link"
-import Image from 'next/image'
-import { zodResolver } from "@hookform/resolvers/zod"
-import * as z from "zod"
-import { useForm } from "react-hook-form"
-import { Button } from "@/components/ui/button"
-import {
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { Separator } from "@/components/ui/separator"
+type FormValues = {
+    username: string;
+    password: string;
+};
 
-const formSchema = z.object({
-    username: z.string().min(3, {
-        message: "Username must be at least 2 characters.",
-    }),
-    password: z.string().min(8, {
-        message: "password must be at least 8 characters.",
-    }).refine((value) => {
-        // Check for at least one uppercase letter
-        return /[A-Z]/.test(value);
-    }, {
-        message: 'Password must contain at least one capital letter.',
-    }).refine((value) => {
-        // Check for at least one lowercase letter
-        return /[a-z]/.test(value);
-    }, {
-        message: 'Password must contain at least one small letter.',
-    }).refine((value) => {
-        // Check for at least one special character
-        return /[!@#$%^&*()_+{}\[\]:;<>,.?~\-=/\\|]/.test(value);
-    }, {
-        message: 'Password must contain at least one special character.',
-    }),
-})
+const schema = z.object({
+    username: z.string().nonempty("Username is required"),
+    password: z.string().min(8, 'Password must be at least 8 characters.')
+        .refine((value) => /[A-Z]/.test(value), 'Password must contain at least one capital letter.')
+        .refine((value) => /[a-z]/.test(value), 'Password must contain at least one small letter.')
+        .refine((value) => /[!@#$%^&*()_+{}\[\]:;<>,.?~\-=/\\|]/.test(value), 'Password must contain at least one special character.')
+        .refine((value) => /\d/.test(value), 'Password must contain at least one number.')
+});
 
-export default function AuthForm() {
-    // 1. Define your form.
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
+const ZodYouTubeForm = () => {
+    const form = useForm<FormValues>({
+        defaultValues: { //in default values , you can also put here something from an api
             username: "",
+            password: '',
         },
-    })
+        resolver: zodResolver(schema),
+        mode: "onTouched" // this causes validaiton when u type , there are option to choose 
+        //you can also do async validaiton
+    });
 
-    // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof formSchema>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values)
-    }
+    const { register, handleSubmit, formState, control } = form;
+
+    //when we are not using zod we use register to register different form feilds 
+    // const name = register("username") , now the name is an object with various variables in it lets destructre it
+    // const { ref, onChange, onBlur, value } = name;
+    //you can add these as input props like this <input id="username" {...register("username")} (if you are doing this you do not need to define it like this const name = register("username") above  /> or you can add them individually like this  <input id="username" ref={ref} onChange={onChange} onBlur={onBlur}  /> this enables react hook form to track the input feilds and validate them
+
+    //form also has a watch object which helps us to watch any feild 
+
+    //there are two methods getvalues and setvalues which can get you values of the specified feilds when some specific action is performed such as clicking 
+    const { errors, isDirty, isValid } = formState; // there are also other values we can destructure like isSumbiting , isSumbited , isSumbitSuccessfull , sumbitcount
+
+    //  you can also reset the form with reset funciton , ---> useeffect ke andr , if isSumbitsuccessfull then reset , and watch for [insumbitsuccessfull]
+
+    const onSubmit = (data: FormValues) => {
+        console.log(data);
+
+        //you can console log this to see what is the structure of the data being recieved from the form
+        //also suppose the api accept the data in some other form or structure then also you can modify data here before sending it to the api with nested objects you can group some properties checkout video 13 , insted of nested objects you can also use arrays to group some properties video 14
+
+        //sometimes you do not how many feilds in array are required , for example in a website we have an option to add as many phone numbers as the user wants by clicking a plus sign for thisw watch video 15
+    };
+    // you can also disable some feilds , for example disable = "true" or disable = watch("username")==="" ---> in this code if the username is empty then the password feild will be disabled
     return (
-        <div className="flex justify-center items-center flex-col h-screen border-8">
-            <Image className="pb-8" src={'/icon/Twitter-Logo.png'} alt="Twitter Logo" width={70} height={70} />
-            <div className="mb-4 text-2xl font-bold">Sign in To Twitter</div>
-            <Form {...form} >
-                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                    <FormField
-                        control={form.control}
-                        name="username"
-                        render={({ field }) => (
-                            <FormItem>
+        <div className="border-solid border-4 flex flex-col h-screen justify-center items-center">
 
-                                <div className="flex flex-col items-center ">
-                                    <button className="block rounded-full bg-white border-solid border-2 py-3 px-8  my-1" type="submit">
-                                        <div className="flex justify center">
-                                            <Image className="pr-2" src={'/icon/Google-Logo.png'} alt="Google Logo" width={30} height={30} />
-                                            <div className="text-black">Sign in With Google</div>
-                                        </div>
-                                    </button>
-                                    <button className="block rounded-full bg-white border-solid border-2 py-3 px-8  my-4" type="submit">
-                                        <div className="flex justify center">
-                                            <Image className="pr-2" src={'/icon/Github-Logo.png'} alt="Github Logo" width={30} height={30} />
-                                            <div className="text-black">Sign in With Github</div>
-                                        </div>
-                                    </button>
-                                    <Separator className="my-4"></Separator>
+            <Image src="/icon/Twitter-Logo.png" alt="twitter logo" width={100} height={100} />
+            <div className="my-8 text-4xl font-bold">Sign in To Twitter</div>
+            <Button className="rounded-full bg-white text-black border-solid  border-2 my-4"><div className="flex justify center">
+                <Image className="pr-2" src={'/icon/Google-Logo.png'} alt="Google Logo" width={30} height={30} />
+                <div className="text-black">Sign in With Google</div>
+            </div></Button>
+            <Button className="rounded-full bg-white text-black border-solid  border-2 "><div className="flex justify center">
+                <Image className="pr-2" src={'/icon/Github-Logo.png'} alt="Github Logo" width={30} height={30} />
+                <div className="text-black">Sign in With Github</div>
+            </div></Button>
+            <Separator className="w-[40vh] mt-6" />
 
-                                    <FormControl>
-                                        <Input placeholder="Phone , Email or username" className="max-w-xs mb-6" {...field} />
-                                    </FormControl>
-                                    <FormControl>
-                                        <Input placeholder="password" className="max-w-xs" {...field} />
-                                    </FormControl>
-                                    <div className="my-[1rem]">
-                                        <Button className=" rounded-full text-white mt-4" type="submit"><div className="py-3 px-[6rem] text-lg">Login</div></Button>
-                                    </div>
-                                    < Button type="submit" className=" bg-white border-solid border-2 rounded-full" > <div className="text-black">forgot password?</div></Button >
-                                    <div className="mt-4">Don't have an acount? <span className="text-blue-700">Singup</span></div>
-                                    <FormMessage />
+            <form onSubmit={handleSubmit(onSubmit)} noValidate> {/* along with onSumbit , handle sumbit also has a function to handle errors which we get while sumbiting the form */}
+                <div className="flex flex-col items-center ">
+                    <div className="form-control ">
+                        <Input type="text" id="username" placeholder="username , email or phone" className="my-4"{...register("username")} />
+                        <p className="error">{errors.username?.message}</p>
+                    </div>
+                    <div className="form-control">
+                        <Input type="text" id="password" placeholder="password" {...register("password")} />
+                        <p className="error">{errors.password?.message}</p>
+                    </div>
+
+                    <Button className="my-4" disabled={!isDirty || !isValid} >Submit</Button>
+                    {/* here we are using isDirty and isValid to disable the button if the form is not dirty or not valid */}
+                </div>
+            </form>
+
+            <DevTool control={control} />
+            <Button className="rounded-full bg-white text-black border-solid  border-2 px-8 ">
+                <div className="text-black">forgot password</div>
+            </Button>
+            <div className="mt-4">Don't have an acount? <span className="text-blue-700">Singup</span></div>
+        </div>
+        // devtool to visualize react hook form , control is a object destructured from useForm hook and these track the state without rerendering the whole conmpnent while if we have used state the whole componenet would have been re rendered
 
 
-                                </div>
-                            </FormItem>
+    );
+};
 
-                        )
-                        }
-                    />
+export default ZodYouTubeForm;
 
-                </form >
-            </Form >
-        </div >
 
-    )
-}
+//React form hook notes 
 
